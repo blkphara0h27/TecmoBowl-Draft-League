462b97c98e70c42d9fa6199feced48044b7ff559
blkphara0h27 <ejwyatt1470@gmail.com>
2026-03-16 14:50:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -114,6 +114,7 @@ io.on("connection", socket => {
 // ----------------------------
 // SERVER START
 // ----------------------------
-server.listen(PORT, () => {
+const PORT = process.env.PORT || 3000
+  server.listen(PORT, () => {
   console.log("Server running on port " + PORT)
 })
